8400c4e4076d8c1462a26736d51f5ab82e263a16
Mike <michael.p.silverman@gmail.com>
2024-10-05 10:07:43 -0400

    games list and create game form looks ok

--- packages/client/src/app/lobby/games-section/index.tsx
+++ packages/client/src/app/lobby/games-section/index.tsx
@@ -1,19 +1,27 @@
 // @refresh reset
 "use client";
 import { useWebsocketStore } from "@/stores/websocket-store";
-import { FormEvent, useState } from "react";
-import { BASE_SCREEN_SIZE, ClientToServerEvent, GOLDEN_RATIO } from "@speed-dungeon/common";
+import { FormEvent, useEffect, useState } from "react";
+import {
+  BASE_SCREEN_SIZE,
+  ClientToServerEvent,
+  GOLDEN_RATIO,
+  GameListEntry,
+} from "@speed-dungeon/common";
 import ButtonBasic from "../../components/atoms/ButtonBasic";
-import { setAlert } from "../../components/alerts";
-import { useAlertStore } from "@/stores/alert-store";
 import { SPACING_REM_LARGE, SPACING_REM_SMALL } from "@/client_consts";
-import TextSubmit from "@/app/components/molocules/TextSubmit";
+import Divider from "@/app/components/atoms/Divider";
+import { useLobbyStore } from "@/stores/lobby-store";
 
 export default function GamesSection() {
   const socketOption = useWebsocketStore().socketOption;
   const [gameName, setGameName] = useState("");
-  const [gameListRefreshedAt, setGameListRefreshedAt] = useState(Date.now());
-  const mutateAlertStore = useAlertStore().mutateState;
+  const [gameListRefreshedAt, setGameListRefreshedAt] = useState("...");
+  const gameList = useLobbyStore().gameList;
+
+  useEffect(() => {
+    setGameListRefreshedAt(new Date(Date.now()).toLocaleTimeString());
+  }, []);
 
   function createGame(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -22,47 +30,65 @@ export default function GamesSection() {
 
   function refreshGameList() {
     socketOption?.emit(ClientToServerEvent.RequestsGameList);
+    setGameListRefreshedAt(new Date(Date.now()).toLocaleTimeString());
   }
+
+  const testGames = new Array(4).fill("").map((item) => {
+    const entry = {
+      gameName: "some game nameeeeeeeeeeeee",
+      numberOfUsers: Math.floor(Math.random() * 3),
+      timeStarted: null,
+    };
+
+    return <GameListItem game={entry} />;
+  });
+
+  console.log(testGames);
 
   return (
     <div
       id="games-section"
-      className="w-full h-full flex"
+      className="w-full h-full flex flex-wrap flex-col-reverse justify-end laptop:flex-row"
       style={{
         padding: `${SPACING_REM_LARGE}rem`,
-        width: `calc(${BASE_SCREEN_SIZE}px * ${Math.pow(GOLDEN_RATIO, 2)})`,
+        // width: `calc(${BASE_SCREEN_SIZE}px * ${Math.pow(GOLDEN_RATIO, 2)})`,
+        width: "900px",
         maxWidth: "100%",
       }}
     >
       <div
         id="current-games-container"
-        className="pointer-events-auto w-1/2 h-fit"
-        style={{
-          paddingRight: `${SPACING_REM_SMALL / 2}rem`,
-        }}
+        className="pointer-events-auto h-fit w-full laptop:w-6/12 laptop:pr-2"
       >
+        <div className="mb-2">Last refreshed at {gameListRefreshedAt}</div>
         <div
-          className="text-lg h-10 flex items-center border border-slate-400 bg-slate-700"
+          className="text-lg h-10 flex items-center justify-between border border-slate-400 bg-slate-700 mb-2"
           style={{
             paddingLeft: `${SPACING_REM_SMALL}rem`,
           }}
         >
-          Current Games
+          <div>{gameList.length ? "Current games" : "No current games..."}</div>
+          <ButtonBasic onClick={refreshGameList}>Refresh List</ButtonBasic>
         </div>
-        <ul>
-          <li>Last refreshed at {new Date(gameListRefreshedAt).toLocaleTimeString()}</li>
+        {<Divider />}
+        <ul className="mb-1">
+          {
+            // testGames
+          }
+          {gameList.map((game) => (
+            <GameListItem game={game} key={game.gameName} />
+          ))}
         </ul>
       </div>
-      <div
+      <form
         id="create-game-form"
-        className="pointer-events-auto"
-        style={{
-          paddingLeft: `${SPACING_REM_SMALL / 2}rem`,
-        }}
+        className="pointer-events-auto h-fit w-full laptop:w-6/12 laptop:pl-2"
+        onSubmit={createGame}
       >
-        <form className="flex" onSubmit={createGame}>
+        <div className="mb-2">Host a multiplayer game</div>
+        <div className="flex flex-1 mb-2">
           <input
-            className="bg-slate-700 border border-slate-400 h-10 p-4"
+            className="bg-slate-700 border border-slate-400 h-10 p-4 min-w-0 flex-1"
             type="text"
             name="game name"
             placeholder="Game name..."
@@ -72,8 +98,46 @@ export default function GamesSection() {
           <ButtonBasic buttonType="submit" extraStyles="border-l-0 bg-slate-700">
             CREATE
           </ButtonBasic>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
+  );
+}
+
+interface GameListItemProps {
+  game: GameListEntry;
+}
+
+function GameListItem(props: GameListItemProps) {
+  const socketOption = useWebsocketStore().socketOption;
+  function joinGame() {
+    socketOption?.emit(ClientToServerEvent.JoinGame, props.game.gameName);
+  }
+
+  return (
+    <li className="w-full flex items-center border border-slate-400 mb-2 pointer-events-auto bg-slate-700">
+      <div
+        className={`flex-1 overflow-hidden whitespace-nowrap overflow-ellipsis`}
+        style={{
+          paddingLeft: `${SPACING_REM_SMALL}rem`,
+        }}
+      >
+        {props.game.gameName}
+      </div>
+      <div className="h-10 w-28 flex items-center border-r border-l border-slate-400 pl-4 pr-4">
+        <div className="overflow-hidden whitespace-nowrap overflow-ellipsis">
+          {props.game.numberOfUsers}
+          {" player"}
+          {props.game.numberOfUsers > 1 || props.game.numberOfUsers === 0 ? "s" : ""}
+        </div>
+      </div>
+      <ButtonBasic
+        onClick={joinGame}
+        disabled={typeof props.game.timeStarted === "number"}
+        extraStyles="border-0"
+      >
+        {"Join"}
+      </ButtonBasic>
+    </li>
   );
 }
