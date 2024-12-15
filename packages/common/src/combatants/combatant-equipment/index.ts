97a6b8ebf45b31acc1852d3261ecfdc7249adbdf
Mike <michael.p.silverman@gmail.com>
2024-12-15 11:14:13 -0500

    about to try to change items from composition to inheritance

--- packages/common/src/combatants/combatant-equipment/index.ts
+++ packages/common/src/combatants/combatant-equipment/index.ts
@@ -10,3 +10,7 @@ export * from "./get-slot-item-is-equipped-to.js";
 // - should hold the item separately of the inventory bags
 // - should be consistently accessible by their number (same items each time)
 // - should be limitable by the type of equipment they can hold (shield only, swords only etc)
+// - body armor can have hotswap slots on a trait
+// - combatant getHotswapSlots searches through inherent slots, slots from combatantTraits and slots from equipped items
+// - unequipping an item with a hotswapSlot trait removes the items stored in the slots, and is only allowed if
+// there is room in the inventory
