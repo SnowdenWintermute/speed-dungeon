4679a0d31760383f4c7f2605a7b21577f0d80b02
Mike <michael.p.silverman@gmail.com>
2025-03-31 12:40:17 -0400

    moving action steps to server

---
+++ packages/common/src/assets/skeletons.ts
@@ -0,0 +1,10 @@
+import { CombatantSpecies } from "../combatants/combatant-species.js";
+
+export const SKELETON_FILE_PATHS: Record<CombatantSpecies, string> = {
+  [CombatantSpecies.Humanoid]: "humanoid/humanoid-skeleton.glb",
+  [CombatantSpecies.Dragon]: "monsters/dragon-main-skeleton.glb",
+  [CombatantSpecies.Skeleton]: "monsters/skeleton-main-skeleton.glb",
+  [CombatantSpecies.Velociraptor]: "monsters/velociraptor-main-skeleton.glb",
+  [CombatantSpecies.Elemental]: "monsters/cube-main-skeleton.glb",
+  [CombatantSpecies.Golem]: "monsters/wolf-main-skeleton.glb",
+};
---
+++ packages/server/src/game-server/game-event-handlers/character-uses-selected-combat-action-handler/action-sequence-manager.ts
@@ -0,0 +1,115 @@
+import {
+  ActionTracker,
+  COMBAT_ACTIONS,
+  CombatActionExecutionIntent,
+  CombatantContext,
+  ERROR_MESSAGES,
+  IdGenerator,
+  Milliseconds,
+  NestedNodeReplayEvent,
+} from "@speed-dungeon/common";
+import { ActionSequenceManagerRegistry } from "./action-sequence-manager-registry.js";
+
+export class ActionSequenceManager {
+  private remainingActionsToExecute: CombatActionExecutionIntent[];
+  private currentTracker: null | ActionTracker = null;
+  private completedTrackers: ActionTracker[] = [];
+  private isFinalized: boolean = false;
+  constructor(
+    public id: string,
+    actionExecutionIntent: CombatActionExecutionIntent,
+    public replayNode: NestedNodeReplayEvent,
+    public combatantContext: CombatantContext,
+    public sequentialActionManagerRegistry: ActionSequenceManagerRegistry,
+    private idGenerator: IdGenerator,
+    private trackerThatSpawnedThisActionOption: null | ActionTracker
+  ) {
+    this.remainingActionsToExecute = [actionExecutionIntent];
+  }
+  getIsFinalized() {
+    return this.isFinalized;
+  }
+  markAsFinalized() {
+    this.isFinalized = true;
+  }
+  getNextActionInQueue() {
+    return this.remainingActionsToExecute[this.remainingActionsToExecute.length - 1];
+  }
+  getCurrentTracker() {
+    return this.currentTracker;
+  }
+
+  isCurrentlyProcessing() {
+    return !!this.getCurrentTracker();
+  }
+  isDoneProcessing() {
+    return !this.isCurrentlyProcessing() && this.remainingActionsToExecute.length === 0;
+  }
+  // action children may depend on the outcome of their parent so we must process their parent first
+  populateSelfWithCurrentActionChildren() {
+    const currentActionExecutionIntent = this.currentTracker?.actionExecutionIntent;
+    if (!currentActionExecutionIntent || !this.currentTracker) return;
+    const currentAction = COMBAT_ACTIONS[currentActionExecutionIntent.actionName];
+    const childActionIntentResults = currentAction
+      .getChildren(this.combatantContext, this.currentTracker)
+      .map((action) => {
+        const targets = action.getAutoTarget(this.combatantContext, this.currentTracker);
+        return {
+          actionName: action.name,
+          targets,
+        };
+      });
+
+    const childActionIntents: CombatActionExecutionIntent[] = [];
+    for (const intentResult of childActionIntentResults) {
+      const targetsResult = intentResult.targets;
+      if (targetsResult instanceof Error) {
+        console.error(intentResult.targets);
+        continue;
+      }
+      if (targetsResult === null) {
+        console.error(ERROR_MESSAGES.COMBAT_ACTIONS.INVALID_TARGETS_SELECTED);
+        continue;
+      }
+
+      childActionIntents.push(
+        new CombatActionExecutionIntent(intentResult.actionName, targetsResult)
+      );
+    }
+
+    this.remainingActionsToExecute.push(...childActionIntents.reverse());
+  }
+
+  startProcessingNext(time: { ms: Milliseconds }): Error | ActionTracker {
+    if (this.currentTracker) this.completedTrackers.push(this.currentTracker);
+    const nextActionExecutionIntentOption = this.remainingActionsToExecute.pop();
+    if (!nextActionExecutionIntentOption)
+      return new Error("Tried to process next action but there wasn't one");
+
+    let previousTrackerOption: null | ActionTracker = null;
+    if (this.trackerThatSpawnedThisActionOption) {
+      previousTrackerOption = this.trackerThatSpawnedThisActionOption;
+      this.trackerThatSpawnedThisActionOption = null;
+    } else {
+      previousTrackerOption = this.completedTrackers[this.completedTrackers.length - 1] || null;
+    }
+
+    try {
+      const tracker = new ActionTracker(
+        this,
+        this.sequentialActionManagerRegistry.actionStepIdGenerator.getNextId(),
+        nextActionExecutionIntentOption,
+        previousTrackerOption || null,
+        time.ms,
+        this.idGenerator,
+        previousTrackerOption?.spawnedEntityOption
+      );
+
+      this.currentTracker = tracker;
+
+      return tracker;
+    } catch (err) {
+      return err as unknown as Error;
+    }
+  }
+}
