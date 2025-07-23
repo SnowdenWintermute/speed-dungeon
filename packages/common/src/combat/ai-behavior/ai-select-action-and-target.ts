129fd11b9d6fd24f7b2de9a9d2d5da4323543e54
Mike <michael.p.silverman@gmail.com>
2025-07-23 09:41:33 -0400

    starting to think about threat manager

--- packages/common/src/combat/ai-behavior/ai-select-action-and-target.ts
+++ packages/common/src/combat/ai-behavior/ai-select-action-and-target.ts
@@ -2,7 +2,6 @@ import { CombatActionIntent } from "../index.js";
 import { Combatant } from "../../combatants/index.js";
 import { SpeedDungeonGame } from "../../game/index.js";
 import { CombatActionExecutionIntent } from "../combat-actions/combat-action-execution-intent.js";
-import { AllyAndEnemyBattleGroups } from "../../battle/get-ally-and-enemy-battle-groups.js";
 import { AIBehaviorContext } from "./ai-context.js";
 import { CombatantContext } from "../../combatant-context/index.js";
 import { SelectRandomActionAndTargets } from "./custom-nodes/select-random-action-and-targets.js";
@@ -14,27 +13,33 @@ export function AISelectActionAndTarget(
 ): Error | null | CombatActionExecutionIntent {
   const { combatantProperties: userCombatantProperties } = user;
 
-  /// TESTING AI CONTEXT
   const partyResult = SpeedDungeonGame.getPartyOfCombatant(game, user.entityProperties.id);
   if (partyResult instanceof Error) return partyResult;
   const battleOption = SpeedDungeonGame.getBattleOption(game, partyResult.battleId) || null;
-  const aiContext = new AIBehaviorContext(
+
+  const behaviorContext = new AIBehaviorContext(
     new CombatantContext(game, partyResult, user),
     battleOption
   );
 
-  const targetSelectorNode = new SelectRandomActionAndTargets(aiContext, user, [
+  const targetSelectorNode = new SelectRandomActionAndTargets(behaviorContext, user, [
     CombatActionIntent.Malicious,
   ]);
 
   const targetSelectionTreeSuccess = targetSelectorNode.execute();
   console.log("behavior tree result:", BEHAVIOR_NODE_STATE_STRINGS[targetSelectionTreeSuccess]);
-  console.log("combat action intent selected from behavior tree:", aiContext.selectedActionIntent);
+  console.log(
+    "combat action intent selected from behavior tree:",
+    behaviorContext.selectedActionIntent
+  );
 
-  const actionExecutionIntentOption = aiContext.selectedActionIntent;
+  const actionExecutionIntentOption = behaviorContext.selectedActionIntent;
   if (actionExecutionIntentOption === null)
     throw new Error("unhandled case - ai context did not have a selected actionExecutionIntent");
-  userCombatantProperties.combatActionTarget = actionExecutionIntentOption.targets; // must set their target because getAutoTarget may use it
+
+  // must set their target because getAutoTarget may use it when creating action children or triggered actions
+  // although I think this is already done by the behavior tree
+  userCombatantProperties.combatActionTarget = actionExecutionIntentOption.targets;
 
   return actionExecutionIntentOption;
 }
