913136834079df61c29d37ec7bcbfc618eb74c31
Mike <michael.p.silverman@gmail.com>
2025-12-18 09:05:17 -0500

    about to transition client to use client intent gateway

--- packages/client/src/client-intent-gateway/index.ts
+++ packages/client/src/client-intent-gateway/index.ts
@@ -1,1 +1,13 @@
-// ClientIntentGateway
+import { ClientIntent } from "@speed-dungeon/common";
+
+export interface ClientIntentHandler {
+  handleIntent(clientIntent: ClientIntent): void;
+}
+
+export class ClientIntentGateway {
+  constructor(private readonly intentHandler: ClientIntentHandler) {}
+
+  submit(clientIntent: ClientIntent): void {
+    this.intentHandler.handleIntent(clientIntent);
+  }
+}
