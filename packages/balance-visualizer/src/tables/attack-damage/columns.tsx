61d74885ba8aafcf02f4ab39ba970f120bb719ec
Mike <michael.p.silverman@gmail.com>
2026-08-25 15:04:19 -0400

    about to add accuracy study

--- packages/balance-visualizer/src/tables/attack-damage/columns.tsx
+++ packages/balance-visualizer/src/tables/attack-damage/columns.tsx
@@ -9,7 +9,12 @@ function totalContributionColumn(
 ): DataTableColumn<AttackDamageTableRow> {
   return {
     header,
-    renderCell: (row) => Math.round(row.averageContributingAttributes[attribute].total),
+    renderCell: (row) => {
+      const { averageContributingAttributes } = row;
+      const contributions = averageContributingAttributes[attribute];
+      // return `${Math.round(contributions.inherent)}/${Math.round(contributions.fromGear)}/${Math.round(contributions.allocated)}/${Math.round(contributions.total)}`;
+      return `${Math.round(contributions.total)}`;
+    },
   };
 }
 
